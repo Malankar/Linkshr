32f8ecb9dc8f2d911afaf22c171bca95ff266178
Avdhut <32304774+Malankar@users.noreply.github.com>
2022-09-27 21:28:14 +0530

    Update loginUser.js

--- pages/api/v1/loginUser.js
+++ pages/api/v1/loginUser.js
@@ -3,9 +3,9 @@ import Users from "../../../models/userModel";
 export default async function loginUser(req, res) {
   if (req.method === "POST") {
     if (req.query.apiSecret === process.env.API_SECRET) {
+      await connectMongo();
       const getUser = await Users.find({ email: req.body.email });
       if (getUser) {
-        await connectMongo();
         if (req.body.password == getUser[0].password) {
           console.log("user signed in");
           res.status(200).json({ user: getUser[0] });
